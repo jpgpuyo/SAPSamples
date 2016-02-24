0da76d5b085d5c3a16885d0f16a86f8be8090157
Puyo <joaquim.puyo@atos.net>
2016-02-24 12:19:57 +0100

    added  Android Consumer and Tizen Provider (currently not working on gear S2)

---
+++ HelloAccessory/androidProviderTizenConsumer/HelloAccessoryConsumer/app/src/androidTest/java/com/samsung/android/sdk/accessory/example/helloaccessory/consumer/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.samsung.android.sdk.accessory.example.helloaccessory.consumer;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
